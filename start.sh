f1fda580d9ed88d755ff5fdb0df35ccb843e6020
Sara Fernandez <saradfrz@gmail.com>
2024-03-30 15:44:11 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
-export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://airflow:radioactive@localhost:5432/airflow_db"
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
+export AIRFLOW_HOME=/croptracker && \
+export AIRFLOW__CORE__DAGS_FOLDER=/croptracker/dags && \
+export AIRFLOW_VERSION=2.8.1 && \
+export PYTHON_VERSION=3.10 && \
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://airflow:radioactive@localhost:5432/airflow_db" && \
+export AIRFLOW__CORE__LOAD_EXAMPLES=False && \
 export AIRFLOW__CORE__EXECUTOR=LocalExecutor
 airflow scheduler &
 airflow webserver &
