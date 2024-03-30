6bcd9d4f0f1ee32c1ea57968617a851593fdd9b2
Sara Fernandez <saradfrz@gmail.com>
2024-03-30 14:22:09 -0300

    Update install_airflow.sh

--- init_files/install_airflow.sh
+++ init_files/install_airflow.sh
@@ -4,7 +4,9 @@ export PYTHON_VERSION=3.10
 export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://airflow:radioactive@localhost:5432/airflow_db"
 export AIRFLOW__CORE__LOAD_EXAMPLES=False
 export AIRFLOW__CORE__EXECUTOR=LocalExecutor
+source .venv/bin/activate
 pip install wheel
 pip install psycopg2
 pip install "apache-airflow[crypto,celery,postgres,cncf.kubernetes,docker]==${AIRFLOW_VERSION}" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 airflow db migrate
+airflow users create -u airflow_user -f admin -l admin -r Admin -e admin@airflow.com -p mypassword123
