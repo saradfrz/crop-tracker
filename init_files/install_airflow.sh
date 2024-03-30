270318cba978ac0fc1289935ab6439876319c338
Sara Fernandez <saradfrz@gmail.com>
2024-03-30 14:45:43 -0300

    Update install_airflow.sh

--- init_files/install_airflow.sh
+++ init_files/install_airflow.sh
@@ -1,4 +1,5 @@
-export AIRFLOW_HOME=/home/airflow
+export AIRFLOW_HOME=/croptracker
+export AIRFLOW__CORE__DAGS_FOLDER=/croptracker/dags
 export AIRFLOW_VERSION=2.8.1
 export PYTHON_VERSION=3.10
 export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://airflow:radioactive@localhost:5432/airflow_db"
@@ -10,3 +11,5 @@ pip install psycopg2
 pip install "apache-airflow[crypto,celery,postgres,cncf.kubernetes,docker]==${AIRFLOW_VERSION}" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 airflow db migrate
 airflow users create -u airflow_user -f admin -l admin -r Admin -e admin@airflow.com -p mypassword123
+airflow scheduler &
+airflow webserver &
